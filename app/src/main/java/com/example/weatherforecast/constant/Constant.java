d677b19b463734ce9b09d06070b12d31ed5b3b48
zhangbenzhi <zhangbenzhi@dangkegongyu.com>
2019-04-30 10:17:07 +0800

    修改接口请求key

--- app/src/main/java/com/example/weatherforecast/constant/Constant.java
+++ app/src/main/java/com/example/weatherforecast/constant/Constant.java
@@ -5,10 +5,10 @@ public class Constant {
     //天气请求接口地址：
     public static final String weatherUrl = "http://v.juhe.cn/weather/index";
     //请求天气apikey:
-    public static final String weatherApikey = "90f0a65ba4897116f31e6932625e92e2";
+    public static final String weatherApikey = "7e352726a4a720ba7fa42a476d287940";
     //请求pm:
     public static final String pmUrl = "http://web.juhe.cn:8080/environment/air/pm";
     //请求pm的apikey:
-    public static final String pmApikey = "df3cc1d1e6235e4b15c7fa134e56919c";
+    public static final String pmApikey = "a88593159593dd3b95da1b9f2d488733";
 
 }
